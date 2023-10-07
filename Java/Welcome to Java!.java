a0e386af23a8631bf0f347c7af23bb5b1a961a94
Infinity (KIIT) <64971616+Cosmic-Infinity@users.noreply.github.com>
2023-10-08 03:23:33 +0530

    Create Welcome to Java!.java

---
+++ Java/Welcome to Java!.java
@@ -0,0 +1,12 @@
+//Prepare>Java>Introduction>Welcome to Java!
+//Java 7
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+
