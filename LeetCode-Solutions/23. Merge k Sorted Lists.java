cb31082293f09f48c887ec296d4d5f679b9df352
Shubham Sinha <64971616+Cosmic-Infinity@users.noreply.github.com>
2025-07-14 01:09:23 +0530

    Update 23. Merge k Sorted Lists.java

--- LeetCode-Solutions/23. Merge k Sorted Lists.java
+++ LeetCode-Solutions/23. Merge k Sorted Lists.java
@@ -1,3 +1,50 @@
+//5ms 45.05mb
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import java.util.*;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int k = 0; k < lists.length; k++) {
+            while (lists[k] != null) {
+                pq.offer(lists[k].val);
+                lists[k] = lists[k].next;
+            }
+        }
+        ListNode res = null;
+        ListNode head = res;
+        while (!pq.isEmpty()) {
+            if (res == null) {
+                res = new ListNode();
+                res.val = pq.poll();
+                res.next = null;
+                head = res;
+            } else {
+                res.next = new ListNode();
+                res = res.next;
+                res.val = pq.poll();
+                res.next = null;
+            }
+        }
+        return head;
+    }
+}
+
+
+
+
+/*
 //134ms 44.33mb
 
 /**
@@ -48,3 +95,4 @@ class Solution {
         return start;
     }
 }
+*/
